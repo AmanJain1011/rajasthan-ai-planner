09f949f499b43a4b9eeb743806c57839f79a3cbf
Aman Jain <98634732+AmanJain1011@users.noreply.github.com>
2026-04-01 18:40:40 +0530

    Add constants for cities and categories

---
+++ app/constants.py
@@ -0,0 +1,10 @@
+CITIES = ["Jaipur", "Jodhpur", "Bikaner", "Ajmer", "Udaipur", "Pushkar", "Jaisalmer"]
+
+CATEGORY_MAP = {
+    "attractions": ["tourism.sights", "entertainment"],
+    "food": ["catering.restaurant", "catering.fast_food", "catering.cafe"],
+    "parks": ["leisure.park"],
+    "temples": ["religion"],
+    "museums": ["entertainment.museum"],
+    "hotels": ["accommodation.hotel", "accommodation.guest_house", "accommodation.hostel"]
+}
